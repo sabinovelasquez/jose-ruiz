17bdf2ef7202774b631fa5ad20b6773b95a9e625
Sabino <sabino@Sabinos-MacBook-Pro.local>
2015-07-16 19:23:13 -0300

    flicker api - css - gallery

--- source/js/lib/script.js
+++ source/js/lib/script.js
@@ -1,14 +1,22 @@
-function FlickrPhotoSet(){
-    var apiCall = "https://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157629938135441&per_page=100&page=1&api_key=6d578cf191cfbff7d715f5ee286784b8&jsoncallback=?";
+
+function FlickrPhotoSet(albumId){
+    var apiCall = 'https://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id='+albumId+'&per_page=100&page=1&api_key=6d578cf191cfbff7d715f5ee286784b8&jsoncallback=?';
     $.getJSON(apiCall, function(data){
-        console.log('get pics');
         $.each(data.photoset.photo, function(index,item){
             console.log(item);
             var img_src = "http://farm" + item.farm + ".static.flickr.com/" + item.server + "/" + item.id + "_" + item.secret;
-            var dataB = img_src + "_b.jpg";
+            var dataB = img_src + "_o.jpg";
+            var img = '<div class="col-xs-12 col-sm-4 col-lg-3 item">';
+            img += '<a data = "'+dataB+'" ><div class="hover"></div><img class="img-responsive center-block" src="'+img_src+'.jpg" /></a>';
+            img += '</div>';
             var a_href = "http://www.flickr.com/photos/" + data.photoset.owner + "/" + item.id + "/";
-            $('<img class="img-responsive center-block" />').attr("src", img_src + '_m.jpg').appendTo("#gal").wrap(('<div class="col-xs-12 col-sm-4 col-lg-3"></div>'));
+            $("#gal").append(img);
+        });
+        $('#gal').imagesLoaded( function(){
+            $('#content').isotope({
+                itemSelector : '.item'
+            });
         });
     });
 };
-FlickrPhotoSet();
+FlickrPhotoSet('72157631622073873');
