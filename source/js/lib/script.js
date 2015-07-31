a68ac7568d4166aa3aaaa22dd1498266617a7ead
Sabino <sabino@Sabinos-MacBook-Pro.local>
2015-07-31 11:22:24 -0300

    albums de flickr

--- source/js/lib/script.js
+++ source/js/lib/script.js
@@ -6,34 +6,39 @@ var gsheet = '1oojR3DLLsh6qOmn5Nm_Xl7yzSiPBAaFpccsx8E6Ez-8',
     w=0,
     h=0;
 
-var Caso = function(name, description, flickrId, category, published) {
+var Caso = function() {
     name = this.name;
     description = this.description;
     flickrId = this.flickrId;
     category = this.category;
     published = this.published;
+    photos = this.photos;
 }
-
-function FlickrPhotoSet(albumId){
+var Album = function (){
+    // $.each(data.photoset.photo, function(index,item){
+    //     console.log(item);
+    //     var img_src = "http://farm" + item.farm + ".static.flickr.com/" + item.server + "/" + item.id + "_" + item.secret;
+    //     var dataB = img_src + "_o.jpg";
+    //     var img = '<div class="col-xs-12 col-sm-4 col-lg-3 item">';
+    //     img += '<a data = "'+dataB+'" ><div class="hover"></div><img class="img-responsive center-block" src="'+img_src+'.jpg" /></a>';
+    //     img += '</div>';
+    //     var a_href = "http://www.flickr.com/photos/" + data.photoset.owner + "/" + item.id + "/";
+    //     $("#gal").append(img);
+    // });
+}
+function FlickrPhotoSet(albumId, caso){
     var apiCall = 'https://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id='+albumId+'&per_page=100&page=1&api_key=6d578cf191cfbff7d715f5ee286784b8&jsoncallback=?';
     $.getJSON(apiCall, function(data){
-        $.each(data.photoset.photo, function(index,item){
-            console.log(item);
-            var img_src = "http://farm" + item.farm + ".static.flickr.com/" + item.server + "/" + item.id + "_" + item.secret;
-            var dataB = img_src + "_o.jpg";
-            var img = '<div class="col-xs-12 col-sm-4 col-lg-3 item">';
-            img += '<a data = "'+dataB+'" ><div class="hover"></div><img class="img-responsive center-block" src="'+img_src+'.jpg" /></a>';
-            img += '</div>';
-            var a_href = "http://www.flickr.com/photos/" + data.photoset.owner + "/" + item.id + "/";
-            $("#gal").append(img);
-        });
+        caso.photos = data.photoset.photo;
     });
+
 };
 
 function makeCases(){
     $(casos).each(function(index, item){
-
+        FlickrPhotoSet(item.flickrId, item);
     });
+    console.log(casos);
 }
 
 $.getJSON(url, function(data) {
